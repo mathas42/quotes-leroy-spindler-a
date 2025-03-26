2449fef38fb8841537d4ca86f0111beb3fa0f560
GardeDeNuit <matteo.spindler17@gmail.com>
2025-03-26 16:59:23 +0100

    Initial quotes program

---
+++ quotes.c
@@ -0,0 +1,10 @@
+#include "quotes.h"
+
+void print_quote(char ** quotes, int index) {
+    printf("%s\n", quotes[index]);
+}
+
+void print_random_quote(char ** quotes) {
+    int index = rand() % MAX_QUOTES;
+    print_quote(quotes, index);
+}
